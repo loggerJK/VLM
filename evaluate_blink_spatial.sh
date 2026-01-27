1ba31a017fd556190c9da1a6fde3dcbed0ceb1ea
jinlovespho <msjchr922@gmail.com>
2026-01-27 14:02:10 +0900

    spatial eval

---
+++ evaluate_blink_spatial.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=5 python evaluate_blink_spatial.py \
+    --checkpoint Alpha-VLLM/Lumina-DiMOO \
+    --vae_ckpt Alpha-VLLM/Lumina-DiMOO \
+    --output_dir ./evaluation_results/lora_spatial_base \
+    --steps 20 \
+    --gen_length 20 \
+    --block_length 20 
+    # --lora_ckpt_path /mnt/cvlab22_data1/heeji/dVLM_outputs_from_ext_srv/lora128_spatial_relation_wohead_und-20260126-144357/epoch1-iter34431-step2500 \
+
